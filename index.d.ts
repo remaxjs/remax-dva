580bd9ef77fe689372a1d76f6dee438cc0f67e29
朱政 <43430109@qq.com>
2020-10-16 11:48:11 +0800

    修正缺少 forwardRef 参数

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,8 @@ import {
   ReducersMapObject,
   MiddlewareAPI,
   StoreEnhancer,
-  bindActionCreators
-} from 'redux';
+  bindActionCreators,
+} from "redux";
 
 import { History } from "history";
 
@@ -15,64 +15,67 @@ export interface Dispatch<A extends Action = AnyAction> {
 }
 
 export interface onActionFunc {
-  (api: MiddlewareAPI<any>): void,
+  (api: MiddlewareAPI<any>): void;
 }
 
 export interface ReducerEnhancer {
-  (reducer: Reducer<any>): void,
+  (reducer: Reducer<any>): void;
 }
 
 export interface Hooks {
-  onError?: (e: Error, dispatch: Dispatch<any>) => void,
-  onAction?: onActionFunc | onActionFunc[],
-  onStateChange?: () => void,
-  onReducer?: ReducerEnhancer,
-  onEffect?: () => void,
-  onHmr?: () => void,
-  extraReducers?: ReducersMapObject,
-  extraEnhancers?: StoreEnhancer<any>[],
+  onError?: (e: Error, dispatch: Dispatch<any>) => void;
+  onAction?: onActionFunc | onActionFunc[];
+  onStateChange?: () => void;
+  onReducer?: ReducerEnhancer;
+  onEffect?: () => void;
+  onHmr?: () => void;
+  extraReducers?: ReducersMapObject;
+  extraEnhancers?: StoreEnhancer<any>[];
 }
 
 export type DvaOption = Hooks & {
-  namespacePrefixWarning?: boolean,
-  initialState?: Object,
-  history?: Object,
-}
+  namespacePrefixWarning?: boolean;
+  initialState?: Object;
+  history?: Object;
+};
 
 export interface EffectsCommandMap {
-  put: <A extends AnyAction>(action: A) => any,
-  call: Function,
-  select: Function,
-  take: Function,
-  cancel: Function,
-  [key: string]: any,
+  put: <A extends AnyAction>(action: A) => any;
+  call: Function;
+  select: Function;
+  take: Function;
+  cancel: Function;
+  [key: string]: any;
 }
 
 export type Effect = (action: AnyAction, effects: EffectsCommandMap) => void;
-export type EffectType = 'takeEvery' | 'takeLatest' | 'watcher' | 'throttle';
+export type EffectType = "takeEvery" | "takeLatest" | "watcher" | "throttle";
 export type EffectWithType = [Effect, { type: EffectType }];
 export type Subscription = (api: SubscriptionAPI, done: Function) => void;
-export type ReducersMapObjectWithEnhancer = [ReducersMapObject, ReducerEnhancer];
+export type ReducersMapObjectWithEnhancer = [
+  ReducersMapObject,
+  ReducerEnhancer
+];
 
 export interface EffectsMapObject {
-  [key: string]: Effect | EffectWithType,
+  [key: string]: Effect | EffectWithType;
 }
 
 export interface SubscriptionAPI {
-  history: History,
-  dispatch: Dispatch<any>,
+  history: History;
+  dispatch: Dispatch<any>;
 }
 
 export interface SubscriptionsMapObject {
-  [key: string]: Subscription,
+  [key: string]: Subscription;
 }
 
 export interface Model {
-  namespace: string,
-  state?: any,
-  reducers?: ReducersMapObject | ReducersMapObjectWithEnhancer,
-  effects?: EffectsMapObject,
-  subscriptions?: SubscriptionsMapObject,
+  namespace: string;
+  state?: any;
+  reducers?: ReducersMapObject | ReducersMapObjectWithEnhancer;
+  effects?: EffectsMapObject;
+  subscriptions?: SubscriptionsMapObject;
 }
 
 export interface DvaInstance {
@@ -81,30 +84,21 @@ export interface DvaInstance {
    *
    * @param hooks
    */
-  use: (hooks: Hooks) => void,
+  use: (hooks: Hooks) => void;
 
   /**
    * Register a model.
    *
    * @param model
    */
-  model: (model: Model) => void,
+  model: (model: Model) => void;
 
   /**
    * Unregister a model.
    *
    * @param namespace
    */
-  unmodel: (namespace: string) => void,
-
-  /**
-   * Config router. Takes a function with arguments { history, dispatch },
-   * and expects router config. It use the same api as react-router,
-   * return jsx elements or JavaScript Object for dynamic routing.
-   *
-   * @param router
-   */
-  router: (router: Router) => void,
+  unmodel: (namespace: string) => void;
 
   /**
    * Start the application. Selector is optional. If no selector
@@ -112,7 +106,7 @@ export interface DvaInstance {
    *
    * @param selector
    */
-  start: (selector?: HTMLElement | string) => any,
+  start: (selector?: React.ReactNode, opts?: { forwardRef: boolean }) => any;
 }
 
 export default function dva(opts?: DvaOption): DvaInstance;
@@ -120,6 +114,11 @@ export default function dva(opts?: DvaOption): DvaInstance;
 export { bindActionCreators };
 
 export {
-  connect, connectAdvanced, useSelector, useDispatch, useStore,
-  DispatchProp, shallowEqual
-} from 'react-redux';
+  connect,
+  connectAdvanced,
+  useSelector,
+  useDispatch,
+  useStore,
+  DispatchProp,
+  shallowEqual,
+} from "react-redux";
